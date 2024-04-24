2eaa79c5ae09602e2a1709d0551c00e25a9a893b
jhimi01 <talukderjhimi@gmail.com>
2024-04-24 19:55:02 +0600

    preloader added in right way

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,25 +1,25 @@
 import { lazy, Suspense } from "react";
 import { Outlet } from "react-router-dom";
-
 import Preloader from "../components/Preloader";
-const LazyNavbar = lazy(() => import("../pages/Shared/NavigationBar/NavigationBar"));
+const LazyNavbar = lazy(() =>
+  import("../pages/Shared/NavigationBar/NavigationBar")
+);
 const LazyFooter = lazy(() => import("../pages/Shared/Footer/Footer"));
-const LazyScrollTop = lazy(() => import("../pages/Shared/ScrollToTop/ScrollToTop"));
-
+const LazyScrollTop = lazy(() =>
+  import("../pages/Shared/ScrollToTop/ScrollToTop")
+);
 
 const Main = () => {
-
   return (
     <>
       <Suspense fallback={<Preloader></Preloader>}>
         <div>
-        <LazyNavbar />
-        <Outlet />
-        <LazyScrollTop />
-        <LazyFooter />
+          <LazyNavbar />
+          <Outlet />
+          <LazyScrollTop />
+          <LazyFooter />
         </div>
       </Suspense>
-     
     </>
   );
 };
