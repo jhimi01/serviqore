08aef6f4ae1908b5dfd986ea90f20af88d5918ee
jhimi01 <talukderjhimi@gmail.com>
2024-04-26 19:43:33 +0600

    testimonials

--- src/pages/Homepage/Testimonials/Testimonials.jsx
+++ src/pages/Homepage/Testimonials/Testimonials.jsx
@@ -11,43 +11,45 @@ import { Autoplay, Navigation } from "swiper/modules";
 
 const Testimonials = () => {
   return (
-    <div className="md:w-10/12 overflow-visible relative w-11/12 mx-auto my-10">
-      <h2 className="font-semibold text-[36px] leading-[44px] text-secondary pb-3 border-b-[1px] border-[#B4B4B4] ">
-        Testimonials
-      </h2>
+    <div className="my-10">
+      <div className="md:w-10/12 mx-auto">
+        <h2 className="font-semibold text-[36px] leading-[44px] text-secondary pb-3 border-b-[1px] border-[#B4B4B4] ">
+          Testimonials
+        </h2>
+      </div>
 
-      <div className="mt-8">
+      <div className="testimonials mt-8 md:w-[87%] w-[95%] mx-auto">
         <Swiper
           slidesPerView={1}
           spaceBetween={10}
           centeredSlides={true}
           loop={true}
-          autoplay={{
-            delay: 4500,
-            disableOnInteraction: false,
-          }}
+          // autoplay={{
+          //   delay: 4500,
+          //   disableOnInteraction: false,
+          // }}
           breakpoints={{
             640: {
               slidesPerView: 2,
-              spaceBetween: 20,
+              // spaceBetween: 20,
             },
             768: {
               slidesPerView: 2,
-              spaceBetween: 40,
+              // spaceBetween: 40,
             },
             1024: {
               slidesPerView: 3,
-              spaceBetween: 30,
+              spaceBetween: 0,
             },
           }}
           navigation={true}
           modules={[Autoplay, Navigation]}
-          className="mySwiper relative"
+          className="mySwiper"
         >
-          <SwiperSlide className="w-full">
+          <SwiperSlide className="w-full pl-5 pr-8 pb-10 ">
             <div className="w-full">
               <img src={mark} />
-              <p className="text-justify leading-[18px] text-textMuted text-[15px] font-normal my-2 pl-6">
+              <p className="text-justify leading-[18px] text-textMuted text-[15px] font-normal mt-4 mb-2 pl-3">
                 Working with ServiQore has been an absolute game-changer for our
                 business. Their expertise in digital marketing helped us triple
                 our online visibility within just a few months. The team's
@@ -60,10 +62,10 @@ const Testimonials = () => {
               </h3>
             </div>
           </SwiperSlide>
-          <SwiperSlide className="w-full">
+          <SwiperSlide className="w-full pl-5 pr-8 pb-10">
             <div className="w-full">
               <img src={mark} />
-              <p className="text-justify leading-[18px] text-textMuted text-[15px] font-normal my-2 pl-6">
+              <p className="text-justify leading-[18px] text-textMuted text-[15px] font-normal mt-4 mb-2 pl-3">
                 ServiQore's virtual resourcing services have been instrumental
                 in streamlining our operations. Their team seamlessly integrated
                 with ours, providing exceptional support and contributing to
@@ -76,10 +78,10 @@ const Testimonials = () => {
               </h3>
             </div>
           </SwiperSlide>
-          <SwiperSlide className="w-full">
+          <SwiperSlide className="w-full pl-5 pr-8 pb-10">
             <div className="w-full">
               <img src={mark} />
-              <p className="text-justify leading-[18px] text-textMuted text-[15px] font-normal my-2 pl-6">
+              <p className="text-justify leading-[18px] text-textMuted text-[15px] font-normal mt-4 mb-2 pl-3">
                 We've been working with ServiQore for over a year now, and the
                 results speak for themselves. Their cost-saving initiatives have
                 helped us optimize operational efficiency and redirect resources
@@ -92,10 +94,10 @@ const Testimonials = () => {
               </h3>
             </div>
           </SwiperSlide>
-          <SwiperSlide className="w-full">
+          <SwiperSlide className="w-full pl-5 pr-8 pb-10">
             <div className="w-full">
               <img src={mark} />
-              <p className="text-justify leading-[18px] text-textMuted text-[15px] font-normal my-2 pl-6">
+              <p className="text-justify leading-[18px] text-textMuted text-[15px] font-normal mt-4 mb-2 pl-3">
                 ServiQore's virtual resourcing services have been instrumental
                 in streamlining our operations. Their team seamlessly integrated
                 with ours, providing exceptional support and contributing to
