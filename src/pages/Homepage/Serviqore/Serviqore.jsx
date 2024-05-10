c6eee4221d82c1d0b8f66b96f4820f90cbb5b858
jhimi01 <talukderjhimi@gmail.com>
2024-05-10 20:58:42 +0600

    changed section

--- src/pages/Homepage/Serviqore/Serviqore.jsx
+++ src/pages/Homepage/Serviqore/Serviqore.jsx
@@ -10,20 +10,13 @@ const Serviqore = () => {
         <div className="md:w-1/2">
           <img className="mx-auto my-5" src={group} />
         </div>
-        <div className="md:w-1/2 text-start space-y-5">
+        <div className="md:w-1/2 text-start">
           <h1 className="font-semibold text-2xl sm:text-[30px] text-black leading-[42px]">
             Ready to elevate your security services to the next level?
           </h1>
-          <h1 className="font-semibold text-4xl sm:text-[50px] text-black">
+          <h1 className="font-semibold text-4xl sm:text-[50px] text-black mb-3 md:mt-4 md:mb-7">
             Partner with Serviqore
           </h1>
-          {/* <p className="font-normal my-3 text-[16px] leading-[16px] text-textMuted">
-          Your Trusted Ally in Remote Security Supervision.
-        </p>
-          <p className="font-normal my-3 text-[16px] leading-[16px] text-black">
-            Contact us today to explore how our solutions can support your
-            Security Guard Company's success.
-          </p> */}
           <div><Link to="/contact">
             <button className="flex items-center justify-center gap-2 bg-primary hover:opacity-90 text-white py-2 px-4 rounded mr-auto">
               <span>Get a Quote </span> <FaLongArrowAltRight />
