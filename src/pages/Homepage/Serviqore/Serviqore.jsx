f0d4377de1db227baec6ded4b6ad40e83b2c29d7
jhimi01 <talukderjhimi@gmail.com>
2024-05-10 21:05:38 +0600

    changed section

--- src/pages/Homepage/Serviqore/Serviqore.jsx
+++ src/pages/Homepage/Serviqore/Serviqore.jsx
@@ -11,17 +11,19 @@ const Serviqore = () => {
           <img className="mx-auto my-5" src={group} />
         </div>
         <div className="md:w-1/2 text-start">
-          <h1 className="font-semibold text-2xl sm:text-[30px] text-black leading-[42px]">
+          <h1 className="font-semibold text-2xl sm:text-[30px] leading-[30px] text-black md:leading-[42px]">
             Ready to elevate your security services to the next level?
           </h1>
           <h1 className="font-semibold text-4xl sm:text-[50px] text-black mb-3 md:mt-4 md:mb-7">
             Partner with Serviqore
           </h1>
-          <div><Link to="/contact">
-            <button className="flex items-center justify-center gap-2 bg-primary hover:opacity-90 text-white py-2 px-4 rounded mr-auto">
-              <span>Get a Quote </span> <FaLongArrowAltRight />
-            </button>
-          </Link></div>
+          <div>
+            <Link to="/contact">
+              <button className="flex items-center justify-center gap-2 bg-primary hover:opacity-90 text-white py-2 px-4 rounded mr-auto">
+                <span>Get a Quote </span> <FaLongArrowAltRight />
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
